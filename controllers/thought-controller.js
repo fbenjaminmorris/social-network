2cd5d8ec01c027786d39dfa106aa76b0b6a7afb6
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-09-23 16:52:19 -0400

    main

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -50,4 +50,20 @@ const thoughtController = {
             .catch(err => res.json(err));
         })
         .catch(err => res.status(400).json(err));
-    },   
+    }, 
+    
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
