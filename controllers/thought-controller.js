76de40b3ea85d7d8248c6660ccd4f71e6c10616a
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-09-23 20:24:27 -0400

    main

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -88,3 +88,38 @@ const thoughtController = {
         })
         .catch(err => res.status(500).json(err));
     },
+
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    deleteReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: body.reactionId } } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json({message: 'Successfully deleted the reaction'});
+        })
+        .catch(err => res.status(500).json(err));
+    },
+}
+
+module.exports = thoughtController;
