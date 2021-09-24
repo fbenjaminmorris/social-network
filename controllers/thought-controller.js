d4cb50ab97abe4bd9a4091b5c6cd28421af4f173
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-09-23 20:05:24 -0400

    main

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -67,3 +67,24 @@ const thoughtController = {
         })
         .catch(err => res.status(400).json(err));
     },
+
+    deleteThought({ params }, res) {
+        
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            
+            User.findOneAndUpdate(
+                { username: dbThoughtData.username },
+                { $pull: { thoughts: params.id } }
+            )
+            .then(() => {
+                res.json({message: 'Successfully deleted the thought'});
+            })
+            .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+    },
